e674daad92d6be98c7e8e6fbffd0232074c7c8c1
navies <epoon061@uottawa.ca>
2017-01-19 10:51:11 -0500

    Implement solver class with basic functionality

--- Solver.java
+++ Solver.java
@@ -1,40 +1,78 @@
+import java.util.LinkedList;
+import java.util.Stack;
+import java.util.PriorityQueue;
+
 public class Solver {
-    // We define a state of the game to be the board position, the number of moves to reach the board position, and the previous state
-    // First, insert the initial state (the initial board, 0, moves, and a null previous state)
-    // Then, delete from the priority queue the state with the minimum priority and insert onto the priority queue all neighbouring states
-    // Repeat until the state dequeued is the goal state.
+    /**
+     * Represents a state of the game
+     */
+    private class State implements Comparable<State>{
+        /**
+         * The board position
+         */
+        RandomPermutation position;
+
+        /**
+         * The number of moves to reach the board position
+         */
+        int moves;
+
+        /**
+         * The previous state of the game
+         */
+        State previous;
+
+        public State(RandomPermutation position, int moves, State previous) {
+            this.position = position;
+            this.moves = moves;
+            this.previous = previous;
+        }
+
+        public int compareTo(State state) {
+            return Integer.compare(this.position.manhattan() + this.moves, state.position.manhattan() + state.moves);
+        }
+    }
 
     /**
-     * The current board position
+     * A list of board positions corresponding to a solution to an instance of the game
      */
-    private RandomPermutation current;
-
-    /**
-     * The previous board position
-     */
-    private RandomPermutation previous;
-
-    /**
-     *
-     */
-
+    private Stack<RandomPermutation> solution;
 
     /**
      * Find a solution to the initial board
      */
-    public Solver(RandomPermutation initial[]) {
+    public Solver(RandomPermutation board) {
+        State initial = new State(board, 0, null);
+        PriorityQueue<State> queue = new PriorityQueue<>();
+        queue.add(initial);
+
+        while((queue.peek().position.hamming() != 0)) {
+            State state = queue.remove();
+            for (RandomPermutation neighbour : state.position.neighbours()) {
+                State neighbouringState = new State(neighbour, state.moves + 1, state);
+                queue.add(neighbouringState);
+            }
+        }
+
+        solution = new Stack<>();
+        State state = queue.remove();
+        while(state.previous != null) {
+            solution.push(state.position);
+            state = state.previous;
+        }
     }
 
     /**
      * @return The minimum number of moves to solve the initial board
      */
     public int moves() {
-
+        return solution.size();
     }
 
     /**
      * Returns an Iterable of RandomPermutation positions in solution
      */
     public Iterable<RandomPermutation> solution() {
+        return solution;
     }
 }
